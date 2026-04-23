49d1c1cfb8c2a813f727af32fede132743bcd8b1
aswarina <lyxsyyawa@gmail.com>
2026-04-23 11:23:17 +0700

    logika insert node di posisi paling depan

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -35,6 +35,12 @@ public:
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
         }
+
+        
     }
 }
